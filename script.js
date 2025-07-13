6abfff3a2156634999517650ee041b3a51884be9
Tasnimul Hasan <suniay420@gmail.com>
2025-07-13 22:07:00 +0600

    change song part as select favorite song and add albums

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("run javascript");
 let curentSong = new Audio();
 let crrFolder;
+let crrAlubmsFolders;
 
 function formatTime(seconds) {
   if (isNaN(seconds)) return "0:00";
@@ -45,6 +46,46 @@ async function getSong(folder) {
   return song;
 }
 
+
+async function getSongAlbums(folder) {
+  crrAlubmsFolders = folder;
+  let a = await fetch(`http://127.0.0.1:3000/${folder}/`);
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+
+  // let ahrf = Array.from(div.getElementsByTagName("a"));
+  let ahrf = div.getElementsByTagName("a");
+
+  console.log(ahrf);
+
+  let song = [];
+
+  for (let i = 0; i < ahrf.length; i++) {
+    if (ahrf[i].href.endsWith(".mp3")) {
+      song.push(ahrf[i].href.split(`${crrAlubmsFolders}`)[1]);
+    }
+  }
+
+  // ahrf.forEach((element) => {
+  //   if (element.href.endsWith(".mp3")) {
+  //     song.push(element.href.split("${folder}")[1]);
+  //   }
+  // });
+
+  //also a format for find .mp3 song in file
+  // let song = ahrf
+  // .filter(el => el.href.endsWith(".mp3"))
+  // .map(el => el.href);
+
+  return song;
+}
+
+
+
+
+
+
 const playMusic = (track, pause = false) => {
   if (curentSong) {
     curentSong.pause();
@@ -56,15 +97,25 @@ const playMusic = (track, pause = false) => {
     play.src = "items svg/pause.svg";
   }
 
-  document.querySelectorAll(".track-info").forEach((el) => {
-    if (el.firstElementChild) {
-      el.firstElementChild.innerHTML = decodeURI(track);
-      el.lastElementChild.innerHTML = track
-        .replaceAll("%20", " ")
-        .split("-")[1]
-        .replace(".mp3", "");
-    }
-  });
+  // document.querySelectorAll(".track-info").lastElementChild.forEach((el) => {
+  //   if (el.firstElementChild) {
+  //     el.firstElementChild.innerHTML = decodeURI(track);
+  //     el.lastElementChild.innerHTML = track
+  //       .replaceAll("%20", " ")
+  //       .split("-")[1]
+  //       .replace(".mp3", "");
+  //   }
+  // });
+
+
+  document.querySelector(".now-playing").children[1].firstElementChild.innerHTML= decodeURI(track);
+  document.querySelector(".now-playing").children[1].lastElementChild.innerHTML =
+    track.replaceAll("%20", " ").split("-")[1].replace(".mp3", "");
+  
+  
+  // document.querySelector(".track-info").lastElementChild.innerHTML =
+  //   track.replaceAll("%20", " ").split("-")[1].replace(".mp3", "");
+
 
   // curentSong.addEventListener("timeupdate", () => {
   //   console.log(curentSong.currentTime, curentSong.duration);
@@ -93,55 +144,80 @@ const playMusic = (track, pause = false) => {
       (curentSong.currentTime / curentSong.duration) * 100 + "%";
   };
 
-  // document.querySelector("track-info").lastElementChild.innerHTML=track.replaceAll("%20", " ").split("-")[1].replace(".mp3", "")
+  
 };
 
 async function main() {
   let songs = await getSong("/songs/song_add/");
+  let alubms = await getSongAlbums("/songs/albums/tahsan/");
   console.log(songs);
+  console.log(alubms);
 
   console.log(curentSong.src);
+  
+  let rendomSongSelect = Math.round(Math.random() * (songs.length - 1));
+  console.log(rendomSongSelect);
 
-  let songAdd = document.querySelector(".songList").firstElementChild;
-  console.log(songAdd);
-  for (const song of songs) {
-    // songAdd.innerHTML = songAdd.innerHTML+`<li>${decodeURIComponent(song)}</li>`;
-    // songAdd.innerHTML = songAdd.innerHTML+`<li>${song.replace(/%20/g," ")}</li>`;
-    let songu = song.replaceAll("%20", " ");
-    songAdd.innerHTML =
-      songAdd.innerHTML +
-      `
-        <li>
-            <div class=" songNameInfo flex">
-                <img class="invert-color song_icon" src="items svg/music.svg" alt="">
-                <div>
-                    <div class="track-title" >${songu}</div>
-                    <div class="track-artist">${songu
-                      .split("-")[1]
-                      .replace(".mp3", "")}</div>
-                </div>
-            </div>
 
-                <img class="invert-color" src="items svg/play.svg" alt="">
-        </li>
-        `;
-  }
+  // let songAdd = document.querySelector(".songList").firstElementChild;
+
+
+
+
+  let songAdd = document.getElementById("songAdd");
+
+  let songs_adds = "";
+  
+for (const song of songs) {
+  let songu = song.replaceAll("%20", " ");
+
+  let songTitle = songu;
+  let songArtist = songu.split("-")[1]?.replace(".mp3", "") || "Unknown Artist";
+  songs_adds += `
+    <div class="track-card">
+      <div class="track-image"></div>
+      <div class="track-info">
+        <div class="track-title">${songTitle}</div>
+        <div class="track-artist">${songArtist}</div>
+      </div>
+    </div>
+  `;
+}
+
+  songAdd.innerHTML = songs_adds;
+
+    //         <li>
+    //     <div class=" songNameInfo flex">
+    //         <img class="invert-color song_icon" src="items svg/music.svg" alt="">
+    //         <div>
+    //             <div class="track-title" >${songu}</div>
+    //             <div class="track-artist">${songu
+    //               .split("-")[1]
+    //               .replace(".mp3", "")}</div>
+    //         </div>
+    //     </div>
+
+    //         <img class="invert-color" src="items svg/play.svg" alt="">
+    // </li>
+  
+
 
   //atuch song
 
   Array.from(
-    document.querySelector(".songList").getElementsByTagName("li")
-  ).forEach((e) => {
+    document.querySelector(".track-grid").querySelectorAll(".track-card")
+  ).forEach((e,index) => {
     e.addEventListener("click", () => {
+      rendomSongSelect = index;
       playMusic(
-        e.querySelector(".songNameInfo").lastElementChild.firstElementChild
+        e.querySelector(".track-info").firstElementChild
           .innerHTML
       );
+      console.log('song clicked:', e.querySelector(".track-title").innerHTML, index);
+      
     });
   });
 
-  let rendomSongSelect = Math.round(Math.random() * (songs.length - 1));
-  console.log(rendomSongSelect);
 
   playMusic(songs[rendomSongSelect], true); //play first song
 
@@ -187,7 +263,6 @@ async function main() {
     document.querySelector(".progress").style.width = percentage * 100 + "%";
   });
 
-
   let clamped = 0.5; // default volume level
   curentSong.volume = clamped; // set initial volume
   document.querySelector(".volume-level").style.width = "50%";
@@ -218,8 +293,6 @@ async function main() {
       document.querySelector(".volume-icon img").src = "items svg/volume.svg";
     }
   });
-
-
 
   let invertImg = false;
   // hamburger menu show and hide
@@ -252,7 +325,6 @@ async function main() {
   });
 
   forward_song.addEventListener("click", () => {
-    console.log("previous song clicked");
     // let index = songs.indexOf(
     //   curentSong.src.split("${folder}")[1]
     // );
